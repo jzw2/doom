26746c4c870c8def4eaffa930d33d9123e9ce9ae
= <jzw2@illinois.edu>
2021-09-03 18:56:11 -0500

    Change font or somtehing

--- config.el
+++ config.el
@@ -19,9 +19,17 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+ ;; (setq doom-font (font-spec :family "noto" :size 12 :weight 'semi-light)
+;; doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;;
+;;
 
+(setq doom-font (font-spec :family "Noto Sans Mono" :size 16)
+      ;; doom-big-font (font-spec :family "JetBrains Mono" :size 36)
+      doom-variable-pitch-font (font-spec :family "Noto Sans" :size 24)
+      ;; doom-unicode-font (font-spec :family "JuliaMono")
+      ;; doom-serif-font (font-spec :family "IBM Plex Mono" :weight 'light)
+      )
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
