c7af5f5eda9e05fc811aeb206210d12d593a6c5f
John Wang <jzw2@illinois.edu>
2022-08-28 02:16:59 +0200

    Change email

--- config.el
+++ config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "John Wang"
-      user-mail-address "jzw2@illinois.edu")
+      user-mail-address "johnz.wang@outlook.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
