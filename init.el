64f237b6eb8ed405fdf1b9847d7b0f4f9ffcf97f
John Wang <jzw2@illinois.edu>
2022-12-22 18:49:50 +0100

    Add ein

--- init.el
+++ init.el
@@ -95,7 +95,7 @@
        ;;docker
        ;;
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        (lookup +docsets)              ; navigate your code and its documentation
